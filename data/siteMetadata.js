3a818e3afdcfafbdaa8a0ab53a9b8bfed524d384
fabrice madre <stepwiseltd@gmail.com>
2024-01-12 21:46:50 +0400

    GT analytics update (wip)

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -33,7 +33,7 @@ const siteMetadata = {
     //   posthogProjectApiKey: '', // e.g. 123e4567-e89b-12d3-a456-426614174000
     // },
     googleAnalytics: {
-      googleAnalyticsId: 'GTM-NXNVNC42', // e.g. G-XXXXXXX
+      googleAnalyticsId: 'GT-PJ4NCG6', // e.g. G-XXXXXXX
     },
   },
   newsletter: {
